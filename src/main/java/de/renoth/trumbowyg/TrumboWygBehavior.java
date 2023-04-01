b6af6af21f5f8b702639fae965c8864dad75e0c2
renoth <johannes@renoth.dev>
2023-04-01 22:46:33 +0200

    Initial commit

---
+++ src/main/java/de/renoth/trumbowyg/TrumboWygBehavior.java
@@ -0,0 +1,88 @@
+package de.renoth.trumbowyg;
+
+import java.util.List;
+
+import org.apache.wicket.Application;
+import org.apache.wicket.Component;
+import org.apache.wicket.behavior.Behavior;
+import org.apache.wicket.markup.head.CssHeaderItem;
+import org.apache.wicket.markup.head.HeaderItem;
+import org.apache.wicket.markup.head.IHeaderResponse;
+import org.apache.wicket.markup.head.JavaScriptHeaderItem;
+import org.apache.wicket.markup.head.OnDomReadyHeaderItem;
+import org.apache.wicket.request.cycle.RequestCycle;
+import org.apache.wicket.request.handler.resource.ResourceReferenceRequestHandler;
+import org.apache.wicket.request.resource.CssResourceReference;
+import org.apache.wicket.request.resource.JavaScriptResourceReference;
+import org.apache.wicket.request.resource.PackageResourceReference;
+import org.apache.wicket.request.resource.ResourceReference;
+import org.apache.wicket.resource.JQueryResourceReference;
+
+/**
+ * TODO
+ */
+public class TrumboWygBehavior extends Behavior {
+
+	@Override
+	public void renderHead(Component component, IHeaderResponse response) {
+		super.renderHead(component, response);
+
+		if (component == null) {
+			throw new IllegalStateException("TrumboWygBehavior is not bound to a component");
+		}
+		else {
+			response.render(
+					JavaScriptHeaderItem.forReference(
+							new TrumbowygJavaScriptResourceReference(
+									TrumboWygBehavior.class,
+									"../../../../webjars/trumbowyg/2.27.3/dist/trumbowyg.js")));
+			response.render(
+					CssHeaderItem.forReference(
+							new CssResourceReference(
+									TrumboWygBehavior.class,
+									"../../../../webjars/trumbowyg/2.27.3/dist/ui/trumbowyg.css")));
+
+			response.render(new OnDomReadyHeaderItem(getInitScript(component)));
+		}
+	}
+
+	private String getInitScript(Component component) {
+		final var handler = new ResourceReferenceRequestHandler(
+				new PackageResourceReference(
+						TrumboWygBehavior.class,
+						"../../../../webjars/trumbowyg/2.27.3/dist/ui/icons.svg"));
+		final var svgUrl = RequestCycle.get().urlFor(handler).toString();
+		return String.format("$.trumbowyg.svgPath = '%1$s';$('#%2$s').trumbowyg();", svgUrl, component.getMarkupId());
+	}
+
+	@Override
+	public void bind(Component component) {
+		super.bind(component);
+
+		component.setOutputMarkupId(true);
+	}
+
+	private static class TrumbowygJavaScriptResourceReference extends JavaScriptResourceReference {
+
+		public TrumbowygJavaScriptResourceReference(Class<?> scope, String name) {
+			super(scope, name);
+		}
+
+		@Override
+		public List<HeaderItem> getDependencies() {
+			ResourceReference backingLibraryReference;
+
+			if (Application.exists()) {
+				backingLibraryReference = Application.get().getJavaScriptLibrarySettings().getJQueryReference();
+			}
+			else {
+				backingLibraryReference = JQueryResourceReference.getV3();
+			}
+
+			final var dependencies = super.getDependencies();
+			dependencies.add(JavaScriptHeaderItem.forReference(backingLibraryReference));
+
+			return dependencies;
+		}
+	}
+}
